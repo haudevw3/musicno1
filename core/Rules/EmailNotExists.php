16f167b3960fbc5529524621ee5c141c5052cf38
haudevw3 <hau.nguyen.devw3@gmail.com>
2024-11-03 11:52:41 +0700

    Update code Rules.

---
+++ core/Rules/EmailNotExists.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\Rules;
+
+use Illuminate\Contracts\Validation\Rule as RuleContract;
+
+class EmailNotExists implements RuleContract
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed   $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $user = app(\Modules\User\Repository\UserRepository::class)
+                ->findOne(['email' => str_filter($value)]);
+
+        return ! is_null($user) ? true : false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string|array
+     */
+    public function message()
+    {
+        return 'Tên email không tồn tại. Vui lòng kiểm tra lại thông tin.';
+    }
+}
