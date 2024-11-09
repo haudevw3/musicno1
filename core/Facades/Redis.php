fadc5bcc7b241efe545cab5e3cc88cd9ebfb68f1
haudevw3 <hau.nguyen.devw3@gmail.com>
2024-11-09 17:57:17 +0700

    Update code.

--- core/Facades/Redis.php
+++ core/Facades/Redis.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static bool   has(string $key)
  * @method static mixed  get(string $key)
  * @method static bool   connected()
+ * @method static mixed  flush()
  * @method static \Core\Redis\Contracts\PhpRedisConnection  set(string $key, mixed $value)
  * @method static \Core\Redis\Contracts\PhpRedisConnection  delete(array|string $keys)
  * @method static \Core\Redis\Contracts\PhpRedisConnection  push(string $key, mixed $value)
