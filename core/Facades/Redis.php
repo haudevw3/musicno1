948fc7d8c7855252734e7222f5497bace06356c8
haudevw3 <hau.nguyen.devw3@gmail.com>
2024-11-09 10:03:25 +0700

    Update code.

---
+++ core/Facades/Redis.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Core\Facades;
+
+use Core\Redis\Contracts\PhpRedisConnection;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static bool   has(string $key)
+ * @method static mixed  get(string $key)
+ * @method static bool   connected()
+ * @method static \Core\Redis\Contracts\PhpRedisConnection  set(string $key, mixed $value)
+ * @method static \Core\Redis\Contracts\PhpRedisConnection  delete(array|string $keys)
+ * @method static \Core\Redis\Contracts\PhpRedisConnection  push(string $key, mixed $value)
+ * @method static \Core\Redis\Contracts\PhpRedisConnection  connection($name = null)
+ * @method static \Illuminate\Redis\Connections\PhpRedisConnection|null getConnection()
+ *
+ * @see \Core\Redis
+ */
+class Redis extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return PhpRedisConnection::class;
+    }
+}
